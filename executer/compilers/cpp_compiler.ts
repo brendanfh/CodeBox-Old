218aecc07a25da3a35b73f5d752b23278457ab15
Brendan Hansen <brendan.f.hansen@gmail.com>
2018-10-30 16:46:55 -0500

    Specifying c++14

--- executer/compilers/cpp_compiler.ts
+++ executer/compilers/cpp_compiler.ts
@@ -15,6 +15,7 @@ export class CPPCompiler extends BaseCompiler {
 			sourceFile.file_path,
 			"./executer/compilers/secure/seccomp.c",
 			"-lseccomp",
+			"-std=c++14",
 			"-o",
 			execFile.file_path
 		]);
